68a7ee4fdfc9979d2b81e0cd4c81d43682247596
Muhammad Afuzarahman <theafuza@gmail.com>
2023-10-07 21:40:36 -0700

    Create initial API

---
+++ PokemonReviewApp/Repositories/PokemonRepository.cs
@@ -0,0 +1,41 @@
+﻿using PokemonReviewApp.Data;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Repositories
+{
+    public class PokemonRepository: IPokemonRepository
+    {
+        private readonly DataContext _context;
+
+        public PokemonRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public Pokemon GetPokemon(int id)
+        {
+            return _context.Pokemons.Where(p => p.Id == id).FirstOrDefault();
+        }
+
+        public Pokemon GetPokemon(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public decimal GetPokemonRating(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<Pokemon> GetPokemons() 
+        {
+            return _context.Pokemons.OrderBy(p => p.Id).ToList();
+        }
+
+        public bool PokemonExists(int pokeId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
