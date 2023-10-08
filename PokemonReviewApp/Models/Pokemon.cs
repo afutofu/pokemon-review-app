d0b8295c6b5e747f7ae846afc815eee5b23a3660
Muhammad Afuzarahman <theafuza@gmail.com>
2023-10-07 19:16:21 -0700

    Add model many-many relations

--- PokemonReviewApp/Models/Pokemon.cs
+++ PokemonReviewApp/Models/Pokemon.cs
@@ -6,5 +6,7 @@
         public string Name { get; set; }
         public DateTime Birthday { get; set; }
         public ICollection<Review> Reviews { get; set; }
+        public ICollection<PokemonOwner> PokemonOwners { get; set; }
+        public ICollection<PokemonCategory> PokemonCategories { get; set; }
     }
 }
